3e234aca3c81134aad043952897c42c56a47b68d
Luccas <73291149+srluccasonline@users.noreply.github.com>
2023-06-26 21:12:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,43 +6,25 @@ const app = express();
 app.use(json());
 app.use(cors());
 
-let database_number = [];
-
-app.get("/", (req, res) => {
-  res.send("OK Server")
-})
-
-// Endpoint SSE "/realtime"
-app.get('/realtime', (req, res) => {
+app.get('/sse', (req, res) => {
   res.setHeader('Content-Type', 'text/event-stream');
   res.setHeader('Cache-Control', 'no-cache');
   res.setHeader('Connection', 'keep-alive');
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Transfer-Encoding', 'chunked');
 
-  // Envia o valor atual do array para o cliente
-  res.write(`data: ${JSON.stringify(database_number)}\n\n`);
-
-  // Mantém a conexão aberta e atualiza o cliente com novos valores
-  const intervalId = setInterval(() => {
-    res.write(`data: ${JSON.stringify(database_number)}\n\n`);
+  // Enviar o tempo a cada segundo
+  const interval = setInterval(() => {
+    const currentTime = new Date().toLocaleTimeString();
+    res.write(`data: ${currentTime}\n\n`);
   }, 1000);
 
-  // Encerra a conexão quando o cliente se desconectar
-  req.on('close', () => {
-    clearInterval(intervalId);
-
-  });
-});
-
-// Endpoint POST "/change"
-app.post('/change', (req, res) => {
-  const randomNum = Math.floor(Math.random() * 10); // Gera um número aleatório de 0 a 9
-  database_number.push(randomNum); // Adiciona o número gerado ao array
-
-  // Envia uma resposta de sucesso ao cliente
-  res.status(200).json({ message: 'Number added successfully' });
+  // Finalizar a conexão após 10 segundos (apenas para este exemplo)
+  setTimeout(() => {
+    clearInterval(interval);
+    res.end();
+  }, 10000);
 });
 
 app.listen(3000, () => {
-  console.log('SERVIDOR RODANDO!');
+  console.log('Servidor escutando na porta 3000');
 });
